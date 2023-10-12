3680c02ac239f5d0f8006cbfd80a21f2f2dc2715
Sonia Barros <sonianbarros@terra.com.br>
2023-10-12 15:26:18 -0300

    components dinamycs propertys

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./small-card.component.css']
 })
 export class SmallCardComponent implements OnInit {
+
+@Input()
+  photoCover: string="https://sobresagas.com.br/wp-content/uploads/2020/12/Disney.jpg"
+@Input()
+  cardTitle: string="NOVA SÉRIE ANUNCIADA!"
 
   constructor() { }
 
